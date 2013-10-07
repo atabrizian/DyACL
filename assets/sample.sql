dc35c4eab383a10d23ed946a3c1875fe9cf8fe43
Arash Tabriziyan <a.tabriziyan@gmail.com>
2013-10-08 01:06:43 +0330

    Sample correction

--- assets/sample.sql
+++ assets/sample.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 28, 2013 at 10:14 PM
+-- Generation Time: Oct 07, 2013 at 07:29 PM
 -- Server version: 5.5.32-MariaDB-log
 -- PHP Version: 5.5.4
 
