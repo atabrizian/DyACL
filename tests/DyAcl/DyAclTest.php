90fc1bcc3b84cdac284a0fd0b8eef011ab24b0e0
Arash Tabriziyan <a.tabriziyan@gmail.com>
2013-10-24 20:53:17 +0330

    some tests for DyAclPDO

--- tests/DyAcl/DyAclTest.php
+++ tests/DyAcl/DyAclTest.php
@@ -15,6 +15,11 @@ use DyAcl\DyAcl;
  */
 class DyAclTest extends \PHPUnit_Framework_TestCase {
 
+    private $sampleHost = "localhost";
+    private $sampleDbName = "DyACL";
+    private $sampleUsername = "testUsername";
+    private $samplePassword = "testPassword";
+
     /**
      * This test is supposed to set rules oith "setRule" command and test isAllowed
      */
@@ -59,7 +64,7 @@ class DyAclTest extends \PHPUnit_Framework_TestCase {
     {
         $dyacl = new DyAcl();
 
-//        $dyacl->setRole('user');//it's better to use user_id and here it'- not needed
+//        $dyacl->setRole('user');//it's better to use user_id and here it's not needed
 
         $dyacl->allow('public');//it's better to use resource_id
         $dyacl->allow('user_can_not_delete');
@@ -69,5 +74,41 @@ class DyAclTest extends \PHPUnit_Framework_TestCase {
 
         $dyacl->forceDeny('user_can_not_delete', DyAcl::ACTION_DELETE);
         $this->assertFalse($dyacl->isAllowed('user_can_not_delete', DyAcl::ACTION_DELETE));
+    }
+
+    public function testAddResource()
+    {
+        $dyacl = new DyAclPDO("mysql:host={$this->sampleHost};dbname={$this->sampleDbName};", $this->sampleUsername, $this->samplePassword);
+        $this->assertTrue($dyacl->addResourceToDB('newResource'));
+    }
+
+    public function testAddUserRoleToDB()
+    {
+        $dyacl = new DyAclPDO("mysql:host={$this->sampleHost};dbname={$this->sampleDbName};", $this->sampleUsername, $this->samplePassword);
+        $this->assertTrue($dyacl->addUserRoleToDB(4, 4));
+    }
+
+    public function testAddRuleToDB()
+    {
+        $dyacl = new DyAclPDO("mysql:host={$this->sampleHost};dbname={$this->sampleDbName};", $this->sampleUsername, $this->samplePassword);
+        $this->assertTrue($dyacl->addRuleToDB(2, 5, DyAcl::ACTION_DELETE));
+    }
+
+    public function testRemoveResourceFromDB()
+    {
+        $dyacl = new DyAclPDO("mysql:host={$this->sampleHost};dbname={$this->sampleDbName};", $this->sampleUsername, $this->samplePassword);
+        $this->assertTrue($dyacl->removeResourceFromDB(5));
+    }
+
+    public function testRemoveUserRoleFromDB()
+    {
+        $dyacl = new DyAclPDO("mysql:host={$this->sampleHost};dbname={$this->sampleDbName};", $this->sampleUsername, $this->samplePassword);
+        $this->assertTrue($dyacl->removeUserRoleFromDB(4, 4));
+    }
+
+    public function testRemoveRuleFromDB()
+    {
+        $dyacl = new DyAclPDO("mysql:host={$this->sampleHost};dbname={$this->sampleDbName};", $this->sampleUsername, $this->samplePassword);
+        $this->assertTrue($dyacl->removeRuleFromDB(2, 5, $dyacl::ACTION_DELETE, $dyacl::ALLOW));
     }
 }
